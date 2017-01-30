f9a1fc8ffd19912913693d0a38aff13da119b7a2
KEBEY JEAREY-CHRIS <3308670@ppti-14-303-06.ufr-info-p6.jussieu.fr>
2017-01-30 16:25:49 +0100

    Rendu1.1

--- tools.py
+++ tools.py
@@ -1,3 +1,24 @@
+from soccersimulator import Vector2D, SoccerState, SoccerAction
+from soccersimulator import Simulation, SoccerTeam, Player, show_simu
+from soccersimulator import Strategy
+from soccersimulator import settings
+import math
+
+GAME_WIDTH = 150                # Longueur du terrain
+GAME_HEIGHT = 90                # Largeur du terrain
+GAME_GOAL_HEIGHT = 10           # Largeur des buts
+PLAYER_RADIUS = 1.              # Rayon d un joueur
+BALL_RADIUS = 0.65              # Rayon de la balle
+MAX_GAME_STEPS = 2000           # duree du jeu par defaut
+maxPlayerSpeed = 1.             # Vitesse maximale d un joueur
+maxPlayerAcceleration = 0.2     # Acceleration maximale
+maxBallAcceleration = 5         # Acceleration maximale de la balle
+
+#__Nos constantes
+GOAL = [Vector2D(0,GAME_HEIGHT//2),Vector2D(GAME_WIDTH,GAME_HEIGHT//2)]
+
+
+
 class MyState(object):
     def __init__(self,state,idteam,idplayer):
         self.state = state
